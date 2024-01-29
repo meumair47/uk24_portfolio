e443651200ef110e6b9124afb02d07854e3eaa49
Muhammad Umair <135255034+meumair47@users.noreply.github.com>
2024-01-30 01:12:59 +0500

    Update SkillCard.jsx

--- src/layouts/SkillCard.jsx
+++ src/layouts/SkillCard.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const SkillCard = (props) => {
   return (
-    <div className=" flex flex-col items-center bg-white hover:bg-black hover:text-white transition-all hover:-translate-y-2 lg:w-1/4 py-5 gap-4 cursor-pointer rounded-md">
-      <h2 className=" text-xl font-semibold text-brightColor">{props.title}</h2>
-      <p>{props.para}</p>
+    <div className="flex flex-col items-center bg-white hover:bg-black hover:text-white transition-all hover:-translate-y-2 w-full lg:w-1/4 py-5 gap-4 cursor-pointer rounded-md">
+      <h2 className="text-lg lg:text-xl font-semibold text-brightColor">{props.title}</h2>
+      <p className="px-3">{props.para}</p>
     </div>
   );
 };
 
 export default SkillCard;
+
