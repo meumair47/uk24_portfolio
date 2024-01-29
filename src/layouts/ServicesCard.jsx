030a6594cc68188fb5c42f81932e32ecb530fa3d
Muhammad Umair <135255034+meumair47@users.noreply.github.com>
2024-01-30 01:34:55 +0500

    Update ServicesCard.jsx

--- src/layouts/ServicesCard.jsx
+++ src/layouts/ServicesCard.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
 const ServicesCard = (props) => {
   return (
-    <div className="flex flex-col items-center bg-white hover:bg-black hover:text-white transition-all hover:-translate-y-2 w-full lg:w-1/4 py-5 gap-4 cursor-pointer rounded-md">
+    <div className='flex flex-col items-center bg-white hover:bg-black hover:text-white transition-all hover:-translate-y-2 w-full lg:w-1/4 py-5 gap-4 cursor-pointer rounded-md'>
       <div>
         <span>{props.icon}</span>
       </div>
@@ -10,6 +10,6 @@ const ServicesCard = (props) => {
       <p className="text-center px-3">{props.para}</p>
     </div>
   );
-};
+}
 
 export default ServicesCard;
